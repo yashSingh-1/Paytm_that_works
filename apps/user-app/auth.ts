44966a91614f438dec8b203d52d6e6d4c5f609ab
Yash Singh <dobbyneedsclothes@gmail.com>
2024-10-21 10:35:54 +0530

    fixing middleware

--- apps/user-app/auth.ts
+++ apps/user-app/auth.ts
@@ -1,12 +1,10 @@
 
 import NextAuth from "next-auth"
-import credentials from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { client } from "@repo/db/client"
-import GitHub from "next-auth/providers/github"
 import authConfig from "./auth.config"
  
-export const { handlers: {GET, POST}, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(client),
   session: { strategy: "jwt"},
   ...authConfig
